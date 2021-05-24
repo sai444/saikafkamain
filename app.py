2acb0e7f23d26d78e59e00b39be4080e71fbd673
sai444 <31222470+sai444@users.noreply.github.com>
2021-05-24 16:57:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,20 @@
 import os
-from flask import Flask, render_template, abort, url_for, json, jsonify
-import json
-import html
-import requests
+import time
 import subprocess
 
 
-url = "http://122.166.167.113:8085/emp"
-
-payload={}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
 
 
 
-
-app = Flask(__name__)
-
-# read file
-
-
-@app.route("/")
-def index():
+def main():
+    print('main function')
     result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
-    list = (response.text)
+   
     print(result.stdout)
   
 # Using for loop
-    for i in list:
-        print(i)
-    return render_template('index.html', title="page", jsonfile=(result.stdout))
 
-app.run(host='localhost', debug=True)
+
+while True :
+    main()
+    time.sleep(10)
